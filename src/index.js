7024e0e6b3c863fa899ddeb33b5ac986a7f7a3ec
DrSavauG <dr.savaug@gmail.com>
2021-02-22 17:16:26 +0300

    map+some+includes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let brackets = bracketsConfig.map((item) => item.join(""));
+    while (brackets.some((el) => str.includes(el))) {
+        brackets.forEach((el) => (str = str.replace(el, "")));
+    }
+    return !str;
+};
+
+// // ////#region
+//  module.exports = function check(str, bracketsConfig) {
+//     const brackets = bracketsConfig.flatMap((item) => item.join(""));
+
+//     for (let index = 0; index < brackets.length; index++) {
+//         if (str.includes(brackets[index])) {
+//             str = str.replace(brackets[index], "");
+//             index = -1;
+//         }
+//     }
+
+//     return str === "";
+// };

f096db3a4662dcd3a6b20687ffcce6168a99ccaa
DrSavauG <dr.savaug@gmail.com>
2021-02-22 17:19:28 +0300

    map+some+includes -comments()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
-    let brackets = bracketsConfig.map((item) => item.join(""));
-    while (brackets.some((el) => str.includes(el))) {
-        brackets.forEach((el) => (str = str.replace(el, "")));
+    let brackets = bracketsConfig.map(item => item.join(""));
+    while (brackets.some(el => str.includes(el))) {
+        brackets.forEach(el => (str = str.replace(el, "")));
     }
     return !str;
 };
